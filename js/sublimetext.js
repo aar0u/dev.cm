2202dc0905692943e6d283801d5f9b9c9081a5a4
aaron <duskymagus@msn.com>
2016-04-22 10:36:07 +0800

    only load json 1 time

--- js/sublimetext.js
+++ js/sublimetext.js
@@ -1,74 +1,78 @@
 ﻿$(function() {
-	var os = '';
+    var os = '';
+    var shortcutsData;
 
-	function loadJsonData(path) {
-		$.getJSON(path, function(data) {
-			updateContent(data);
-		});
-	}
+    function loadJsonData() {
+        if (!shortcutsData) {
+            $.getJSON('js/shortcuts.json', function(data) {
+                shortcutsData = data;
+                updateContent(shortcutsData);
+            });
+        } else {
+            updateContent(shortcutsData);
+        }
+    }
 
-	function updateContent(data){
-			assetData('.element.text-manipulation', data.textManipulation);
-			assetData('.element.navigation', data.navigation);
-			assetData('.element.find-replace', data.findReplace);
-			assetData('.element.general', data.general);
-			assetData('.element.bookmarks', data.bookmarks);
-			assetData('.element.split-window', data.splitWindow);
-			assetData('.element.tabs', data.tabs);
-			assetData('.element.editing', data.editing);
-			assetData('.element.code-folding', data.codeFolding);
-	}
+    function updateContent(data) {
+        assetData('.element.text-manipulation', data.textManipulation);
+        assetData('.element.navigation', data.navigation);
+        assetData('.element.find-replace', data.findReplace);
+        assetData('.element.general', data.general);
+        assetData('.element.bookmarks', data.bookmarks);
+        assetData('.element.split-window', data.splitWindow);
+        assetData('.element.tabs', data.tabs);
+        assetData('.element.editing', data.editing);
+        assetData('.element.code-folding', data.codeFolding);
+    }
 
-	function assetData(elementseKeyword, ds) {
-		$(elementseKeyword).each(function(index, ele) {
-			var $ele = $(ele),
-				data = ds.data,
-				cate = ds.category;
-			$ele.find('.category')[0].innerHTML = cate;
-			if (getParam('lang') === 'en') {
-				$ele.find('.desc.jp')[0].innerHTML = data[index].descJp;
-				$ele.find('.command, .desc.en').text(data[index].descEn);
-			} else {
-				$ele.find('.command, .desc.en').text(data[index].descCn);
-			}
-			$ele.find('.keypress').html(data[index]['keypress' + os]);
-		});
-	}
+    function assetData(elementseKeyword, ds) {
+        $(elementseKeyword).each(function(index, ele) {
+            var $ele = $(ele),
+                data = ds.data,
+                cate = ds.category;
+            $ele.find('.category')[0].innerHTML = cate;
+            if (getParam('lang') === 'en') {
+                $ele.find('.desc.jp')[0].innerHTML = data[index].descJp;
+                $ele.find('.command, .desc.en').text(data[index].descEn);
+            } else {
+                $ele.find('.command, .desc.en').text(data[index].descCn);
+            }
+            $ele.find('.keypress').html(data[index]['keypress' + os]);
+        });
+    }
 
-	$('.element').click(function() {
-		var $self = $(this).hasClass('active');
-		$('.periodic-table').find('.active').removeClass('active').find('.info').fadeOut(300);
-		if (!$self) {
-			$self = $(this).addClass('active');
-			var $info = $self.find('.info');
-			$info.fadeIn(300);
+    $('.element').click(function() {
+        var $self = $(this).hasClass('active');
+        $('.periodic-table').find('.active').removeClass('active').find('.info').fadeOut(300);
+        if (!$self) {
+            $self = $(this).addClass('active');
+            var $info = $self.find('.info');
+            $info.fadeIn(300);
 
-			var container = $self.parents('.container');
-			var pw = container.width() + container.offset().left;
-			var iw = $info.width() + $info.offset().left;
-			if (iw >= pw) {
-				$info.addClass('align-right');
-			}
-		}
-	});
+            var container = $self.parents('.container');
+            var pw = container.width() + container.offset().left;
+            var iw = $info.width() + $info.offset().left;
+            if (iw >= pw) {
+                $info.addClass('align-right');
+            }
+        }
+    });
 
-	$('#osBtnGroup').find('a').click(function(e) {
-		e.preventDefault();
-		$('#osBtnGroup').find('li').removeClass('active');
-		$(this).parent().addClass('active');
-		os = $(this).data().os === 'mac' ? 'Mac' : '';
-		var path = 'js/winData.json';
-		loadJsonData(path);
-	});
+    $('#osBtnGroup').find('a').click(function(e) {
+        e.preventDefault();
+        $('#osBtnGroup').find('li').removeClass('active');
+        $(this).parent().addClass('active');
+        os = $(this).data().os === 'mac' ? 'Mac' : '';
+        loadJsonData();
+    });
 
-	var path = 'js/winData.json';
-	loadJsonData(path);
+    loadJsonData();
 });
 
 function getParam(key) {
-	var re = new RegExp(key + '=(.*?)(?=&|$)');
-	var match = document.URL.match(re);
-	if (match){
-		return match[1];
-	}
+    var re = new RegExp(key + '=(.*?)(?=&|$)');
+    var match = document.URL.match(re);
+    if (match) {
+        return match[1];
+    }
 }
